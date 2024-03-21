0118291b75b14de83ef605cc4558b1b5cc9c8998
joantony <Josselin.ANTONY@etu.uca.fr>
2024-03-21 14:59:16 +0100

    Pagination et recuperationslivres

--- src/components/Livres.vue
+++ src/components/Livres.vue
@@ -5,7 +5,12 @@
 
     <div v-if="livres && livres.length">
       <ul>
-        <li v-for="livre in livres" :key="livre.id">{{ livre.attributes.title }}</li>
+        <li v-for="livre in livres" :key="livre.id">
+          <h3><strong>Titre : </strong>{{ livre.attributes.title }}</h3>
+          <p><strong>Auteur : </strong>{{ livre.attributes.author }}</p>
+          <p>Date de publication : {{ livre.attributes.release_date }}</p>
+          <img :src="livre.attributes.cover" alt="Image du livre" />
+        </li>
       </ul>
     </div>
     <div v-else>
@@ -21,23 +26,46 @@ export default {
   name: 'Livres',
   data() {
     return {
-      livres: [] // Variable de données pour stocker les livres
+      livres: []
     }
   },
-  // Méthode fetchLivres() pour récupérer les livres depuis l'API
   created() {
-    axios.get('https://api.potterdb.com/v1/books')
-        .then(response => {
-          console.log(response.data); // Afficher les données récupérées dans la console
-          this.livres = response.data.data; // Accéder correctement aux données des livres
-        })
-        .catch(error => {
-          console.error('Erreur lors de la récupération des livres : ', error);
-        });
+    this.fetchLivres();
+  },
+  methods: {
+    fetchLivres(url = 'https://api.potterdb.com/v1/books', pageSize = 20) {
+      axios.get(url, {
+        params: {
+          'page[size]': pageSize
+        }
+      })
+          .then(response => {
+            console.log(response.data); // Afficher les données récupérées dans la console
+            this.livres = response.data.data; // Accédez correctement aux données des livres
+            this.pagination.prev = response.data.links.prev;
+            this.pagination.next = response.data.links.next;
+
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+          })
+          .catch(error => {
+            console.error('Erreur lors de la récupération des livres : ', error);
+          });
+    },
+
+    loadPreviousPage() {
+      if (this.pagination.prev) {
+        this.fetchLivres(this.pagination.prev);
+      }
+    },
+    loadNextPage() {
+      if (this.pagination.next) {
+        this.fetchLivres(this.pagination.next);
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
-@import url('../style.css');
+  @import url('../style.css');
 </style>
