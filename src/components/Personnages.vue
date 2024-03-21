bc783c047ac03577479b9921d36ea52140b10639
joantony <Josselin.ANTONY@etu.uca.fr>
2024-03-21 15:22:51 +0100

    Pagination et récuoérations éléments personnages

--- src/components/Personnages.vue
+++ src/components/Personnages.vue
@@ -5,37 +5,84 @@
 
     <div v-if="personnages && personnages.length">
       <ul>
-        <li v-for="personnage in personnages" :key="personnage.id">{{ personnage.attributes.name }}</li>
+        <li v-for="personnage in personnages" :key="personnage.id">
+          <h3>{{ personnage.attributes.name }}</h3>
+          <p>{{ generateBiography(personnage.attributes) }}</p>
+          <img :src="personnage.attributes.image" alt="Image du personnage" />
+        </li>
       </ul>
+      <div>
+        <button @click="loadPreviousPage" :disabled="!pagination.prev">Page Précédente</button>
+        <button @click="loadNextPage" :disabled="!pagination.next">Page Suivante</button>
+      </div>
     </div>
-
     <div v-else>
-      <p>Aucun personnages trouvé.</p>
+      <p>Aucun personnage trouvé.</p>
     </div>
-
-
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+
 export default {
   name: 'Personnages',
-  data(){
-    return{
-      personnages:[]// Variable de données pour stocker les personnages
+  data() {
+    return {
+      personnages: [], // Variable de données pour stocker les personnages
+      pagination: {
+        prev: null,
+        next: null
+      }
     }
   },
-  // Méthode fetchPersonnages() pour récupérer les personnages depuis l'API
-  created(){
-    axios.get('https://api.potterdb.com/v1/characters')
-        .then(response => {
-          console.log(response.data); // Afficher les données récupérées dans la console
-          this.personnages = response.data.data; // Accéder correctement aux données des personnages
-        })
-        .catch(error => {
-          console.error('Erreur lors de la récupération des personnages : ', error);
-        });
+  created() {
+    this.fetchPersonnages();
+  },
+  methods: {
+    fetchPersonnages(url = 'https://api.potterdb.com/v1/characters', pageSize = 20) {
+      axios.get(url, {
+        params: {
+          'page[size]': pageSize
+        }
+      })
+          .then(response => {
+            console.log(response.data); // Afficher les données récupérées dans la console
+            this.personnages = response.data.data; // Accéder correctement aux données des personnages
+            this.pagination.prev = response.data.links.prev;
+            this.pagination.next = response.data.links.next;
+
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+          })
+          .catch(error => {
+            console.error('Erreur lors de la récupération des personnages : ', error);
+          });
+    },
+    loadPreviousPage() {
+      if (this.pagination.prev) {
+        this.fetchPersonnages(this.pagination.prev);
+      }
+    },
+    loadNextPage() {
+      if (this.pagination.next) {
+        this.fetchPersonnages(this.pagination.next);
+      }
+    },
+    generateBiography(attributes) {
+      // Remplacer les valeurs null par "inconnu"
+      for (const key in attributes) {
+        if (attributes[key] === null) {
+          attributes[key] = 'inconnu';
+        }
+      }
+
+      return `${attributes.name || 'inconnu'}, né le ${attributes.born || 'inconnu'} à ${attributes.born || 'inconnu'},
+est un(e) ${attributes.species || 'inconnu'} de nationalité ${attributes.nationality || 'inconnue'}.
+Il est ${attributes.alias_names && attributes.alias_names.length > 0 ? attributes.alias_names.join(', ') : 'inconnu'},
+et a exercé le/les métier(s), dont ${attributes.jobs && attributes.jobs.length > 0 ? attributes.jobs.join(', ') : 'inconnu'}.
+${attributes.name ? attributes.name : 'Il'} était affilié(e) à la maison ${attributes.house || 'inconnue'} à Poudlard.`;
+
+    }
   }
 }
 </script>
